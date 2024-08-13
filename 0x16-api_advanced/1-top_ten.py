638e27bfbe4540663ba3f74475e3a62621d923a1
nomorezhanje23 <nomorezhanje23@gmail.com>
2024-08-13 20:55:41 +0000

    test

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python3
-'''
-    this module contains the function top_ten
-'''
+""" Queries the Reddit API and returns the number of subscribers """
 import requests
-from sys import argv
 
 
-def top_ten(subreddit):
-    '''
-        returns the top ten posts for a given subreddit
-    '''
-    user = {'User-Agent': 'Lizzie'}
-    url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
-                       .format(subreddit), headers=user).json()
+def top_ten(subreddit: str) -> None:
+    """
+    Queries the Reddit API and prints the titles of the first 10 hot posts
+
+    Args:
+        subreddit (str): subreddit to query
+
+    Returns:
+        None
+    """
+    base_url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    request = requests.get(
+        base_url, headers={'User-Agent': 'Agent Uche'}, allow_redirects=False
+    )
+    data = request.json()
     try:
-        for post in url.get('data').get('children'):
-            print(post.get('data').get('title'))
+        for i in range(10):
+            print(data['data']['children'][i]['data']['title'])
     except Exception:
-        print(None)
-
-
-if __name__ == "__main__":
-    top_ten(argv[1])
+        print('None')
